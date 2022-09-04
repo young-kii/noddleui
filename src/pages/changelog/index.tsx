5d7949603c47feb96488355155dba431b6495515
顾逸轩 <535323902@qq.com>
2022-09-05 01:07:05 +0800

    update

--- src/pages/changelog/index.tsx
+++ src/pages/changelog/index.tsx
@@ -2,157 +2,26 @@ import {Config, useTranslation, LocaleConfig} from "@/noddle-components/globalCo
 import Locales from "@/locales";
 import CodeBox from "@/noddle-components/codeBox";
 
-let code = `import STYLE from './index.module.less';
-import './index.less';
-import {useEffect, useRef, useState} from "react";
+let code = `import React from 'react';
+import { Button, Space } from 'tdesign-react';
 
-const keywords = new Set(['var', 'import', 'export', 'let', 'default', 'function', 'from', 'const', 'return']);
-const punctuation = new Set(['(', ')', '{', '}', '=', '<', '>', '[', ']', ':', '/']);
-const tags = new Set(['Cascader', 'div']);
-const special = ['React']
-type props = {
-    code: string
-}
-
-export default (props: props) => {
-    const code = props?.code || ''
-    const codeRef = useRef() as any
-    let codeArray = code.split('')
-    let functionSet = new Set()
-    const compile = (getResult: boolean) => {
-        let currentArray = '', newArray = ''
-        let markCount = 0
-        let inTag = false
-        let inCurlyBracket = false
-        let result = codeArray.reduce((previousValue, currentValue, currentIndex, array) => {
-            if (!getResult) {
-                // if (currentValue === '(') {
-                //     if (currentArray.trim()) functionSet.add(currentArray.trim().split(' ').at(-1))
-                // }
-                let reg = /((var|const|let)\\s*(?<arrowFun>\\S*)\\s*=\\s*\\(.*\\))|(\\s*(?<fun>\\w*)\\s*\\(.*\\))/g
-                let functions = code.matchAll(reg)
-                for (let fun of functions) {
-                    if (fun.groups?.arrowFun)
-                        functionSet.add(fun.groups?.arrowFun)
-                    if (fun.groups?.fun)
-                        functionSet.add(fun.groups.fun)
-                }
-            }
-            if (currentValue === "'" || currentValue === '"') {
-                markCount++
-                if (markCount === 2) {
-                    currentArray += currentValue
-                    newArray = currentArray
-                    currentArray = ''
-                    markCount = 0
-                    return previousValue + \`<span class="mark">\${newArray}</span>\`
-                }
-            }
-            if (markCount === 1) {
-                currentArray += currentValue
-                return previousValue
-            }
-            if (punctuation.has(currentValue)) {
-                let className = 'punctuation'
-                newArray = currentArray
-                if (currentValue === '<') {
-                    inTag = true
-                    className = 'tags'
-                }
-                if (currentValue === '>') {
-                    if (inTag)
-                        className = 'tags'
-                    inTag = false
-                }
-                if (currentValue === '/') {
-                    className = 'tags'
-                }
-                if (currentValue === ':') {
-                    if (!inTag)
-                        newArray = \`<span class="objectKey">\${currentArray}</span>\`
-                }
-                if (currentValue === '=') {
-                    if (inTag)
-                        className = 'mark'
-                }
-                if (currentValue === '{') {
-                    inCurlyBracket = true
-                }
-                if (currentValue === '}') {
-                    inCurlyBracket = false
-                }
-                currentArray = ''
-                return previousValue + newArray + \`<span class="\${className}">\${currentValue}</span>\`
-            } else if (currentValue === ',' || currentValue === ';') {
-                newArray = currentArray
-                currentArray = ''
-                return previousValue + newArray + \`<span class="comma">\${currentValue}</span>\`
-            } else {
-                currentArray += currentValue
-                if (keywords.has(currentArray)) {
-                    if (currentIndex !== array.length - 1 && array[currentIndex + 1] === ' ') {
-                        newArray = currentArray
-                        currentArray = ''
-                        return previousValue + \`<span class="keywords">\${newArray}</span>\`
-                    }
-                } else if (tags.has(currentArray)) {
-                    newArray = currentArray
-                    currentArray = ''
-                    if (array[currentIndex + 1] === '>') {
-                        if (array[currentIndex - newArray.length] === '<' || array[currentIndex - newArray.length] === ' ' || array[currentIndex - newArray.length] === '/')
-                            return previousValue + \`<span class="tags">\${newArray}</span>\`
-                    } else if (array[currentIndex - newArray.length] === '<') {
-                        if (array[currentIndex + 1] === '>' || array[currentIndex + 1] === ' ')
-                            return previousValue + \`<span class="tags">\${newArray}</span>\`
-                    }
-                    return previousValue + \`<span>\${newArray}</span>\`
-                } else if (functionSet.has(currentArray) && (array[currentIndex + 1] === ' ' || punctuation.has(array[currentIndex + 1]))) {
-                    if (inTag) {
-                        if (inCurlyBracket) {
-                            newArray = currentArray
-                            currentArray = ''
-                            return previousValue + \`<span class="functions">\${newArray}</span>\`
-                        }
-                    } else {
-                        newArray = currentArray
-                        currentArray = ''
-                        return previousValue + \`<span class="functions">\${newArray}</span>\`
-                    }
-                } else if (currentArray === '\\n' || currentArray === '\\t') {
-                    currentArray = ''
-                    return previousValue + \`<span>\${currentValue}</span>\`
-                } else if (currentValue === ' ' || currentArray === ' ') {
-                    newArray = currentArray
-                    currentArray = ''
-                    return previousValue + \`<span>\${newArray}</span>\`
-                } else if (currentIndex === array.length - 1) {
-                    return previousValue + currentArray
-                }
-                return previousValue
-            }
-        }, '')
-        if (getResult) return result
-    }
-
-    compile(false)
-    let result = compile(true)
-    useEffect(() => {
-        codeRef.current.innerHTML = result
-    }, [])
-
-    const pre = useRef() as any
-    const show = () => {
-        pre.current.classList.toggle('show')
-    }
-    return <>
-        <button onClick={show}>d</button>
-        <div className={STYLE.codeBoxContainer} ref={pre}>
-            <pre>
-              <code className={STYLE.code} ref={codeRef}/>
-          </pre>
-        </div>
-        hello world
-    </>
+export default function ButtonExample() {
+  return (
+    <Space direction="vertical" style={{ width: '100%' }}>
+      <Button block variant="base">
+        填充按钮
+      </Button>
+      <Button block variant="outline">
+        描边按钮
+      </Button>
+      <Button block variant="dashed">
+        虚框按钮
+      </Button>
+      <Button block variant="text">
+        文字按钮
+      </Button>
+    </Space>
+  );
 }`
 
 export default () => {
