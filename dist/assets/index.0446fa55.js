f02778c2f1813f3bd88aa6bc95b73ee91410953d
young-kii <535323902@qq.com>
2022-09-10 20:05:27 +0800

    remove dist

--- dist/assets/index.0446fa55.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index.92920d2a.js";const n=()=>e("div",{children:"changelog"});export{n as default};
--- dist/assets/index.ada6213f.js
+++
@@ -1,1 +0,0 @@
-import{j as n,O as t}from"./index.92920d2a.js";const o="_content_1i17y_31",e="_container_1i17y_59",c={content:o,container:e},s=()=>n("div",{id:"noddle-components",className:c.container,children:n(t,{})});export{s as default};
