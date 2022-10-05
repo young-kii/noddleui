c9652094110eb19d44c0c2034c3e680871e5d85d
guyixuan <56532982+young-kii@users.noreply.github.com>
2022-10-05 20:04:47 +0800

    fix

--- src/pages/overview/index.tsx
+++ src/pages/overview/index.tsx
@@ -6,27 +6,7 @@ export default () => {
     return (
         <>
             <div style={{width: 400, height: 500, padding: 20}}>
-                <Steps steps={[
-                    {
-                        title: '标题',
-                        content: {
-                            extra: {
-                                content: '2018-6-16',
-                                position: 'footer'
-                            },
-                            main: '这是时间线里面线里面的内线里面的内容sfas线里面的内容sfas线里面的内容sfas容sfas的内容'
-                        }
-                    },
-                    {
-                        title: '标题',
-                        content: {
-                            extra: {
-                                content: '2018-6-16'
-                            },
-                            main: '这是时间线里面的内容'
-                        }
-                    }
-                ]}>123</Steps>
+                
             </div>
             overview
         </>
