ed7d20670a2ce0be98257c84fbe4f4874c87b71e
guyixuan <56532982+young-kii@users.noreply.github.com>
2022-11-08 19:16:00 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,27 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    build: {
-        lib: {
-            entry: resolve(__dirname, 'src/noddle-components/index.tsx'),
-            name: 'noddle-ui',
-            // formats: ['es'],
-            fileName: (format) => `noddle-ui.${format}.js`
-        },
-        rollupOptions: {
-            external: ['react'],
-            output: {
-                // chunkFileNames: 'static/js/[name]-[hash].js',
-                // entryFileNames: 'static/js/[name]-[hash].js',
-                // assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
-                // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
-                globals: {
-                    react: 'React'
-                }
-            }
-        },
-        outDir: 'lib/dist'
-    },
+
     resolve: {
         alias: {
             "@": resolve(__dirname, 'src')
